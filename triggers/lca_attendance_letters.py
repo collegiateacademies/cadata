b4f8731186e746a9a21723c75d58066d617d2560
tophermckee@gmail.com <tophermckee@gmail.com>
2022-09-03 11:00:23 -0500

    why not

---
+++ triggers/lca_attendance_letters.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("..")
+from src.main import *
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%B-%d-%Y %H:%M:%S',
+    filename=f"../logs/{Path(__file__).stem}.log",
+    filemode='w'
+)
+
+generate_attendance_letters('LCA')
