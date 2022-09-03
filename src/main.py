d02f8b022e9baf3b8023f623c622eb51264832c1
tophermckee@gmail.com <tophermckee@gmail.com>
2022-09-03 11:29:36 -0500

    first two requests in there

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from util import *
+import sys
 sys.path.append("..")
+from src.util import *
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -8,3 +9,27 @@ logging.basicConfig(
     filename=f"../logs/{Path(__file__).stem}.log",
     filemode='w'
 )
+
+def generate_attendance_letters(school: string, min_date: string, repeated_letters: bool) -> None:
+    database = {}
+
+    student_list = sr_api_pull(
+        "students",
+        {
+            "active": "1",
+            "school_ids": school_codes[school],
+            'expand': 'grade_level, student_detail' # student_attrs.student_attr_type
+        }
+    )
+
+    absences = sr_api_pull(
+        'absences',
+        parameters = {
+            'active': '1',
+            'school_ids': 15,
+            'min_date': min_date,
+            'max_date': today_yyyy_mm_dd,
+            # 'out_of_school_only': '1',                        # pull everything because you need to include tardies
+            'expand': 'absence_type, student.grade_level'
+        }
+    )
