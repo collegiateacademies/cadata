bc6031751550be07e9c085640644df4346114ede
tophermckee@gmail.com <tophermckee@gmail.com>
2022-09-16 17:57:42 -0500

    good to go

---
+++ triggers/gwc_attendance_letters.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("..")
+from src.main import *
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] %(asctime)s -- %(filename)s on line %(lineno)s\n\tFunction name: %(funcName)s\n\tMessage: %(message)s\n",
+    datefmt='%B-%d-%Y %H:%M:%S',
+    filename=f"../logs/{Path(__file__).stem}.log",
+    filemode='w'
+)
+
+generate_attendance_letters('LCA', '2022-08-01', repeated_letters=True)
