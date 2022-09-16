d2005b481ca4a2cdc3c8398cfca9047b2cb08cef
tophermckee@gmail.com <tophermckee@gmail.com>
2022-09-16 17:02:56 -0500

    debug levels

--- triggers/lca_daily_attendance_email.py
+++ triggers/lca_daily_attendance_email.py
@@ -3,7 +3,7 @@ sys.path.append("..")
 from src.main import *
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="[%(levelname)s] %(asctime)s -- %(filename)s on line %(lineno)s\n\tFunction name: %(funcName)s\n\tMessage: %(message)s\n",
     datefmt='%B-%d-%Y %H:%M:%S',
     filename=f"../logs/{Path(__file__).stem}.log",
