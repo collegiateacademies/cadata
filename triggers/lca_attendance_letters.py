d1668b4798aa50be2d8505b634916087194da0be
tophermckee@gmail.com <tophermckee@gmail.com>
2022-09-19 15:45:36 -0500

    less of a logging mess

--- triggers/lca_attendance_letters.py
+++ triggers/lca_attendance_letters.py
@@ -2,12 +2,4 @@ import sys
 sys.path.append("..")
 from src.main import *
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(levelname)s] %(asctime)s -- %(filename)s on line %(lineno)s\n\tFunction name: %(funcName)s\n\tMessage: %(message)s\n",
-    datefmt='%B-%d-%Y %H:%M:%S',
-    filename=f"../logs/{Path(__file__).stem}.log",
-    filemode='w'
-)
-
 generate_attendance_letters('LCA', '2022-08-01', repeated_letters=True)
