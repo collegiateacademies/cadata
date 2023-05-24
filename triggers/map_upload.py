9aec2746083c115c32a897df9a9f97045426e225
Topher McKee <tophermckee@gmail.com>
2023-05-24 11:08:34 -0500

    comment out initial weird thing

--- triggers/map_upload.py
+++ triggers/map_upload.py
@@ -3,13 +3,13 @@ sys.path.append("..")
 from src.main import *
 
 if __name__ == '__main__':
-    upload_map_file()
+    # upload_map_file()
     try:
         upload_basic(
-        drive_name=f"AssessmentResults{datetime.datetime.now().strftime('%Y-%m-%d %I:%M')}.csv",
-        local_path='AssessmentResults.csv',
-        mimetype='text/csv',
-        folder_id='147y1J5IEVkvMpYtxmTDFI9c1SziYwMXL'
+            drive_name=f"AssessmentResults{datetime.datetime.now().strftime('%Y-%m-%d %I:%M')}.csv",
+            local_path='AssessmentResults.csv',
+            mimetype='text/csv',
+            folder_id='147y1J5IEVkvMpYtxmTDFI9c1SziYwMXL'
     )
     except:
         pass
