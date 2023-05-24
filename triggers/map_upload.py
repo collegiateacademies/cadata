5ca9d01ece2a5733b7a75ad96f6cd429f43636eb
Topher McKee <tophermckee@gmail.com>
2023-05-24 11:00:04 -0500

    uploading file to root first

--- triggers/map_upload.py
+++ triggers/map_upload.py
@@ -4,3 +4,7 @@ from src.main import *
 
 if __name__ == '__main__':
     upload_map_file()
+    try:
+        upload_basic(drive_name=f"AssessmentResults{datetime.datetime.now().strftime('%Y-%m-%d %I:%M')}.csv", local_path='AssessmentResults.csv', mimetype='text/csv')
+    except:
+        pass
