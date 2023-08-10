84a1dae61874ee379c58dda6484871e3d224ae3c
Topher McKee <tophermckee@gmail.com>
2023-08-10 18:35:43 -0500

    trigger for supply wizard name

---
+++ triggers/lca_supply_wizard_staff.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append("..")
+from src.main import *
+
+if __name__ == '__main__':
+    update_typeform_staff_names(
+        typeform_id = 'gx9jMTMg',
+        names_field_id = 'tUEthDKlsDxn'
+    )
