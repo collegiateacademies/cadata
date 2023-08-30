78099078db17bc6cd621f2707ec12bbb34ca22ea
Topher McKee <tophermckee@gmail.com>
2023-08-30 11:22:56 -0500

    have these just send to felter for the first time

--- src/main.py
+++ src/main.py
@@ -782,8 +782,7 @@ def attendance_report(start_date: str = start_date_of_previous_month(), end_date
             html_email = file.read().replace('###table_data###', table_data).replace('###school###', school)
 
         send_email(
-            recipient = 'tophermckee@gmail.com',
-            #cc='afelter@collegiateacademies.org',
+            recipient = 'afelter@collegiateacademies.org',
             subject_line = f'Attendance Report for {school}',
             html_body = html_email,
             sender_string = 'CA Service Provider Reports',
