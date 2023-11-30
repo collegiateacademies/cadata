912dd71b548be82595473c7180789b40985bb06a
Topher McKee <tophermckee@gmail.com>
2023-11-30 16:38:39 -0600

    don't send emails to terminated folks

--- src/main.py
+++ src/main.py
@@ -622,6 +622,8 @@ def send_staff_absence_emails(school: str, all_staff: int, spreadsheet_key: str)
             
             if all_staff == 1:
                 pass
+            elif staff_member[1]: # terminated column
+                pass
             elif all_staff == 0 and email_send == 'FALSE':
                 continue
 
