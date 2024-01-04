2ccb9ece8e125be231b83f87f98268e0145e584f
Topher McKee <tophermckee@gmail.com>
2024-01-04 16:01:10 -0600

    oa new semester

--- triggers/oa_individualized_attendance_reports.py
+++ triggers/oa_individualized_attendance_reports.py
@@ -5,5 +5,5 @@ from src.main import *
 if __name__ == '__main__':
     individualized_attendance_reports(
         school = 'OA',
-        start_date = '2023-08-07',# return_term_start_date('year', 'OA')
+        start_date = '2024-01-08',# return_term_start_date('year', 'OA')
     )
