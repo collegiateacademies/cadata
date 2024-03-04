4998c3b352d1a87689f8c8fb384ff252d95259cb
Topher McKee <tophermckee@gmail.com>
2024-03-04 08:51:21 -0600

    cbr attendance name change

--- src/util.py
+++ src/util.py
@@ -85,16 +85,16 @@ school_info = {
         'fax': '225-286-7808',
         'phone': '225-892-6962',
         'attendance_email': 'info@collegiatebr.org',
-        'attendance_letter_recipient': 'amiles@collegiateacademies.org',
-        'individualized_report_reply': 'amiles@collegiateacademies.org',
+        'attendance_letter_recipient': 'tmunson@collegiateacademies.org,krichardson@collegiateacademies.org',
+        'individualized_report_reply': 'tmunson@collegiateacademies.org,krichardson@collegiateacademies.org',
         'data_manager': 'fsall@collegiateacademies.org',
         'staff_pto_replyto': 'nfranklin@collegiateacademies.org',
         'seat_time': {
             'calendar': False,
             'contact': True,
             'link': '',
-            'contact_name': 'Ava Miles',
-            'contact_email': 'amiles@collegiateacademies.org'
+            'contact_name': 'Tyler Munson and Kimberly Richardson',
+            'contact_email': 'tmunson@collegiateacademies.org,krichardson@collegiateacademies.org'
         }
     },
     'GWC': {
