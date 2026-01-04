6fc7bbe8d7961a6ad2d5be781cfabfb09bc4f8e4
Topher McKee <tophermckee@gmail.com>
2026-01-04 15:57:39 -0600

    updating OA's date for spring 2026

--- triggers/oa_individualized_attendance_reports.py
+++ triggers/oa_individualized_attendance_reports.py
@@ -5,5 +5,5 @@ from src.main import *
 if __name__ == '__main__':
     individualized_attendance_reports(
         school = 'OA',
-        start_date = '2025-08-01',# return_term_start_date('year', 'OA')
+        start_date = '2026-01-05', # return_term_start_date('year', 'OA') UPDATED BY TOPHER ON 2026-01-04
     )
